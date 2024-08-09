5e53bfbc497aa6b39f38fa396f6893e94bccecd9
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-08-09 02:00:10 -0300

    terminando el panel para ver los usuarios y empezando con el admin.js para hacer el crud

---
+++ js/admin.js
@@ -0,0 +1,6 @@
+function agregarUser(){
+    let nombre = document.getElementById("inputNombre").value;
+    let apellido = document.getElementById("inputApellido").value;
+    let dni = document.getElementById("inputDni").value;
+    let numTel = document.getElementById("inputNumTel").value;
+}
