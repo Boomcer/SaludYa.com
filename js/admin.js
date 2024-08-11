f075ed136546f42430c5b62db35b85b53c1fd93f
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-08-11 04:20:56 -0300

    aregando inputs para probar el panel, creando las clases medico y paciente

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,44 @@
-function agregarUser(){
-    let nombre = document.getElementById("inputNombre").value;
-    let apellido = document.getElementById("inputApellido").value;
-    let dni = document.getElementById("inputDni").value;
-    let numTel = document.getElementById("inputNumTel").value;
+class paciente{
+    constructor(nombre,apellido,dni,numTel){
+        this.id = crypto.randomUUID();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.numTel = numTel;
+    }
 }
+
+class medico{
+    constructor(nombre,apellido,dni,numTel,especialidad){
+        this.id = crypto.randomUUID();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.numTel = numTel;
+        this.especialidad = especialidad;
+    }
+}
+
+    let nombre = document.querySelector("#inputNombre");
+    let apellido = document.querySelector("#inputApellido");
+    let dni = document.querySelector("#inputDni");
+    let numTel = document.querySelector("#inputNumTel");
+
+let pacientes = [];
+
+const agregarPaciente = (event) => {
+    event.preventDefault();
+
+    let nuevoPaciente = new paciente( 
+        nombre.value,
+        apellido.value,
+        dni.value,
+        numTel.value,);
+
+    pacientes.push(nuevoPaciente);
+
+    document.querySelector("#formulario-alta").reset();
+
+}
+
+document.querySelector("#formulario-alta").addEventListener('sumbit',agregarPaciente)
