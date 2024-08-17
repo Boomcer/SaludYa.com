f3efc0119f32e55d6db352c53e585a8cf2facd8e
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-08-17 12:32:30 -0300

    agregando el html, css y js del registro, se le hace una par de modificaciones y se le agrega el nav y el footer

--- js/admin.js
+++ js/admin.js
@@ -31,55 +31,26 @@ class admin {
     }
 }
 
-let usuarios = [];
-const guardarUsuarios = () => {
-    localStorage.setItem("users", JSON.stringify(usuarios));
-};
 
 
-let nombre = document.querySelector("#inputNombre");
-let apellido = document.querySelector("#inputApellido");
-let dni = document.querySelector("#inputDni");
-let numTel = document.querySelector("#inputNumTel");
+// crearTabla()
 
+// function crearTabla(){
+//     let users = JSON.parse(localStorage.getItem('users')) || [];
 
-const agregarPaciente = (event) => {
-    event.preventDefault();
+//     users.map((user)=>{
+//         let fila = document.createElement('tr');
+//         let celdas = `
+//         <td>${user.nombre}</td>
+//         <td>${user.apellido}</td>
+//         <td>${user.dni}</td>
+//         <td>${user.numTel}</td>
+//         <td></td>`
 
-    let nuevoPaciente = new paciente( 
-        nombre.value,
-        apellido.value,
-        dni.value,
-        numTel.value,);
+//         fila.innerHTML = celdas;
+//         cuerpoTabla.append(fila);
 
-        usuarios.push(nuevoPaciente);
-        guardarUsuarios();
-
-        document.querySelector("#formulario-alta").reset();
-}
-
-let cuerpoTabla = document.querySelector("tbody");
-
-crearTabla()
-
-function crearTabla(){
-    let users = JSON.parse(localStorage.getItem('users')) || [];
-
-    users.map((user)=>{
-        let fila = document.createElement('tr');
-        let celdas = `
-        <td>${user.nombre}</td>
-        <td>${user.apellido}</td>
-        <td>${user.dni}</td>
-        <td>${user.numTel}</td>
-        <td></td>`
-
-        fila.innerHTML = celdas;
-        cuerpoTabla.append(fila);
-
-    })
-}
-
-document.querySelector("#formulario-alta").addEventListener("submit", agregarPaciente);
+//     })
+// }
 
 
